5922c78ea095cb93a28da2e2bba20f8588cb9c6b
kheni <kheni@VAMBE>
2024-08-30 15:16:09 +0200

    Add project files.

---
+++ CloudPart1/Services/QueueService.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+
+namespace CloudPart1.Services
+{
+    public class QueueService
+    {
+        private readonly QueueServiceClient _queueServiceClient;
+
+        public QueueService(IConfiguration configuration)
+        {
+            _queueServiceClient = new QueueServiceClient(configuration["AzureStorage:ConnectionString"]);
+        }
+
+        public async Task SendMessageAsync(string queueName, string message)
+        {
+            var queueClient = _queueServiceClient.GetQueueClient(queueName);
+            await queueClient.CreateIfNotExistsAsync();
+            await queueClient.SendMessageAsync(message);
+        }
+    }
+
+}
---
+++ CloudPart1/Services/TableService.cs
@@ -0,0 +1,25 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using CloudPart1.Models;
+using System.Threading.Tasks;
+
+namespace CloudPart1.Services
+{
+    public class TableService
+    {
+        private readonly TableClient _tableClient;
+
+        public TableService(IConfiguration configuration)
+        {
+            var connectionString = configuration["AzureStorage:ConnectionString"];
+            var serviceClient = new TableServiceClient(connectionString);
+            _tableClient = serviceClient.GetTableClient("CustomerProfiles");
+            _tableClient.CreateIfNotExists();
+        }
+
+        public async Task AddEntityAsync(CustomerProfile profile)
+        {
+            await _tableClient.AddEntityAsync(profile);
+        }
+    }
+}
